8bff7809652a729708e169a9449121bbf9a34835
BzzzThe18th <69125495+BzzzThe18th@users.noreply.github.com>
2022-04-19 04:45:31 -0700

    Upload source

---
+++ Patches/OnActivatePatch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+
+namespace UmbrellaLock.Patches
+{
+
+    [HarmonyPatch(typeof(UmbrellaItem))]
+    [HarmonyPatch("OnActivate", MethodType.Normal)]
+    class OnActivatePatch
+    {
+        public static bool Prefix()
+        {
+            return Plugin.umbrellaLock;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using System;
+using System.ComponentModel;
+using UnityEngine;
+using Utilla;
+
+namespace UmbrellaLock
+{
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.6.3")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    [Description("HauntedModMenu")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static bool inRoom;
+        public static bool umbrellaLock;
+
+        void OnEnable()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+            umbrellaLock = false;
+        }
+
+        void OnDisable()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+            umbrellaLock = true;
+        }
+
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
+            inRoom = true;
+        }
+
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+            inRoom = false;
+        }
+    }
+}
